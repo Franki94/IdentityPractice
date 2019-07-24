22cc4d0c6637b581c3d054fffaaa35930fc65aea
becker oliver utia lopez <becker_utia_123@hotmail.com>
2019-07-24 11:49:30 -0500

    basic identity creation

---
+++ AspNetIdentityPractice/AspNetIdentityPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace AspNetIdentityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManage = new UserManager<IdentityUser>(userStore);
+
+            var creationResult = userManage.Create(new IdentityUser("becker.utia.123@gmail.com"), "Password123!");
+
+            Console.WriteLine($"Creation {creationResult}");
+            Console.ReadLine();
+        }
+    }
+}
